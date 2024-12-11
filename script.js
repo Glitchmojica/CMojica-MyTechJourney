41c3bac1881460d9a8b14c52a23fb1e1bd7ff15b
Glitchmojica <159349575+Glitchmojica@users.noreply.github.com>
2024-12-11 02:36:33 -0500

    ts

--- script.js
+++ script.js
@@ -85,3 +85,109 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+    // Cursor position and movement
+  let clientX = -100;
+  let clientY = -100;
+  const innerCursor = document.querySelector(".cursor--small");
+
+  const initCursor = () => {
+    document.addEventListener("mousemove", e => {
+      clientX = e.clientX;
+      clientY = e.clientY;
+    });
+
+    const render = () => {
+      innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+      requestAnimationFrame(render);
+    };
+    requestAnimationFrame(render);
+  };
+
+  initCursor();
+
+  // Canvas setup for the noisy circle cursor
+  let lastX = 0;
+  let lastY = 0;
+  let isStuck = false;
+  let group, stuckX, stuckY, bigCoordinates = [];
+
+  const initCanvas = () => {
+    const canvas = document.querySelector(".cursor--canvas");
+    const shapeBounds = { width: 75, height: 75 };
+    paper.setup(canvas);
+    const strokeColor = "rgba(255, 0, 0, 0.5)";
+    const segments = 8;
+    const radius = 15;
+    const noiseScale = 150;
+    const noiseRange = 4;
+    let isNoisy = false;
+
+    const polygon = new paper.Path.RegularPolygon(
+      new paper.Point(0, 0),
+      segments,
+      radius
+    );
+    polygon.strokeColor = strokeColor;
+    polygon.smooth();
+    group = new paper.Group([polygon]);
+    group.applyMatrix = false;
+
+    const noiseObjects = polygon.segments.map(() => new SimplexNoise());
+
+    const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+    const map = (value, in_min, in_max, out_min, out_max) => (
+      ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    );
+
+    paper.view.onFrame = event => {
+      if (!isStuck) {
+        lastX = lerp(lastX, clientX, 0.2);
+        lastY = lerp(lastY, clientY, 0.2);
+        group.position = new paper.Point(lastX, lastY);
+      } else if (isStuck) {
+        lastX = lerp(lastX, stuckX, 0.2);
+        lastY = lerp(lastY, stuckY, 0.2);
+        group.position = new paper.Point(lastX, lastY);
+      }
+
+      if (isStuck && polygon.bounds.width < shapeBounds.width) {
+        polygon.scale(1.08);
+      } else if (!isStuck && polygon.bounds.width > 30) {
+        if (isNoisy) {
+          polygon.segments.forEach((segment, i) => {
+            segment.point.set(bigCoordinates[i][0], bigCoordinates[i][1]);
+          });
+          isNoisy = false;
+          bigCoordinates = [];
+        }
+        polygon.scale(0.92);
+      }
+
+      if (isStuck && polygon.bounds.width >= shapeBounds.width) {
+        isNoisy = true;
+        if (bigCoordinates.length === 0) {
+          polygon.segments.forEach((segment, i) => {
+            bigCoordinates[i] = [segment.point.x, segment.point.y];
+          });
+        }
+
+        polygon.segments.forEach((segment, i) => {
+          const noiseX = noiseObjects[i].noise2D(event.count / noiseScale, 0);
+          const noiseY = noiseObjects[i].noise2D(event.count / noiseScale, 1);
+
+          const distortionX = map(noiseX, -1, 1, -noiseRange, noiseRange);
+          const distortionY = map(noiseY, -1, 1, -noiseRange, noiseRange);
+
+          const newX = bigCoordinates[i][0] + distortionX;
+          const newY = bigCoordinates[i][1] + distortionY;
+
+          segment.point.set(newX, newY);
+        });
+      }
+      polygon.smooth();
+    };
+  };
+
+  initCanvas();
